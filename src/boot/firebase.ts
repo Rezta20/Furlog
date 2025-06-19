c8ec12a36f49408dda540a934e0965451e26359b
rezta20 <mariafu0801@gmail.com>
2025-06-19 22:29:46 +0800

    feat:設定firebase hoisting

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -2,14 +2,13 @@ import { boot } from 'quasar/wrappers';
 import { initializeApp } from 'firebase/app';
 import type { FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBwDtmt3gMHC-XkKEoN5KBvXIyUaN1ysYk',
   authDomain: 'furlog.firebaseapp.com',
   projectId: 'furlog',
-  storageBucket: 'furlog.firebasestorage.app',
+  storageBucket: 'furlog.appspot.com',
   messagingSenderId: '752027687102',
   appId: '1:752027687102:web:5efd0498894b9d389326ef',
   measurementId: 'G-JPBR8L6QNX',
@@ -24,11 +23,15 @@ export default boot(({ app }) => {
   firebaseApp = initializeApp(firebaseConfig);
   auth = getAuth(firebaseApp);
   db = getFirestore(firebaseApp);
-  analytics = getAnalytics(firebaseApp);
-
   app.config.globalProperties.$firebase = firebaseApp;
   app.config.globalProperties.$auth = auth;
   app.config.globalProperties.$db = db;
+
+  if (typeof window !== 'undefined') {
+    void import('firebase/analytics').then(({ getAnalytics }) => {
+      analytics = getAnalytics(firebaseApp);
+    });
+  }
 });
 
 export { firebaseApp, auth, db, analytics };
